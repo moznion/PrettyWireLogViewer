76bca36804d359cb71517099f9b495ed045080a7
moznion <moznion@gmail.com>
2014-10-24 13:17:07 +0900

    Setup external component

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
     jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+    stylish = require('jshint-stylish'),
+    bower = require ('main-bower-files'),
+    gulpFilter = require('gulp-filter'),
+    del = require('del');
 
 gulp.task('test', function () {
     if (typeof process.env.NODE_ENV === 'undefined') {
@@ -15,4 +18,20 @@ gulp.task('lint', function () {
     return gulp.src(['src/*.js'])
         .pipe(jshint())
         .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('bower', ['cleanup-vendor'], function () {
+    var jsFilter = gulpFilter('**/*.js'),
+        cssFilter = gulpFilter('**/*.css');
+
+    return gulp.src(bower())
+        .pipe(jsFilter)
+        .pipe(gulp.dest('vendor/js'))
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe(gulp.dest('vendor/css'));
+});
+
+gulp.task('cleanup-vendor', function () {
+    del.sync('vendor/');
 });
