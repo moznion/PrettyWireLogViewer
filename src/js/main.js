1fcf5d5cb64efc75a8e56872c121e39116c1cf4c
moznion <moznion@gmail.com>
2014-10-24 13:17:59 +0900

    Create view

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+$(function () {
+  var vm = new Vue({
+      el: '#main',
+      data: {
+          'logs': {},
+          'Object': Object,
+      },
+      methods: {
+          getPretty: function () {
+              var self = this;
+              var data = self.$data;
+
+              var wireLogParser = new WireLogParser();
+
+              var logs = {};
+              _(wireLogParser.parse(data.wireLog)).forEach(function (log, index) {
+                  if (log.isEmpty()) {
+                      return;
+                  }
+
+                  var requestLog = '';
+                  var responseLog = '';
+
+                  if (!log.requestLog.isEmpty()) {
+                      requestLog = log.requestLog.toString();
+                  }
+
+                  if (!log.responseLog.isEmpty()) {
+                      responseLog = log.responseLog.toString();
+                  }
+
+                  logs[index] = {
+                      'requestLog': requestLog,
+                      'responseLog': responseLog
+                  };
+              });
+              data.logs = logs;
+          }
+      }
+  });
+});
+
