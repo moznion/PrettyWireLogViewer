9510c1842754e429b77cf6662dd847dde8f73405
moznion <moznion@gmail.com>
2014-10-26 20:47:17 +0900

    Change to parse on ready

--- src/js/main.js
+++ src/js/main.js
@@ -2,48 +2,53 @@
     /* global WireLogParser:false */
     'use strict';
 
-    var vm = new Vue({
-        el: '#main',
-        data: {
-            'logs': {}
-        },
-        methods: {
-            getPretty: function () {
-                var self = this;
-                var data = self.$data;
+    var vm = (function () {
+        var getPretty = function () {
+            var self = this;
+            var data = self.$data;
 
-                // Initialize to redraw when data.logs is changed
-                _(Object.keys(data.logs)).forEach(function (key) {
-                    data.logs.$delete(key);
+            // Initialize to redraw when data.logs is changed
+            _(Object.keys(data.logs)).forEach(function (key) {
+                data.logs.$delete(key);
+            });
+
+            var wireLogParser = new WireLogParser();
+
+            var logs = {};
+            _(wireLogParser.parse(data.wireLog)).forEach(function (log, key) {
+                if (log.isEmpty()) {
+                    return;
+                }
+
+                var requestLog = '';
+                var responseLog = '';
+
+                if (!log.requestLog.isEmpty()) {
+                    requestLog = log.requestLog.toString();
+                }
+
+                if (!log.responseLog.isEmpty()) {
+                    responseLog = log.responseLog.toString();
+                }
+
+                data.logs.$add(key, {
+                    'requestLog': requestLog,
+                    'responseLog': responseLog
                 });
+            });
+        };
 
-                var wireLogParser = new WireLogParser();
-
-                var logs = {};
-                _(wireLogParser.parse(data.wireLog)).forEach(function (log, key) {
-                    if (log.isEmpty()) {
-                        return;
-                    }
-
-                    var requestLog = '';
-                    var responseLog = '';
-
-                    if (!log.requestLog.isEmpty()) {
-                        requestLog = log.requestLog.toString();
-                    }
-
-                    if (!log.responseLog.isEmpty()) {
-                        responseLog = log.responseLog.toString();
-                    }
-
-                    data.logs.$add(key, {
-                        'requestLog': requestLog,
-                        'responseLog': responseLog
-                    });
-                });
-            }
-        }
-    });
+        return new Vue({
+            el: '#main',
+            data: {
+                'logs': {}
+            },
+            methods: {
+                getPretty: getPretty
+            },
+            ready: getPretty
+        });
+    }());
 
     return vm;
 }());
